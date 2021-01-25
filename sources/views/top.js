9c1977e60895f6f86f1271dbdb68e18245412ee9
a_dulsky <andrey.dulsky@gmail.com>
2021-01-25 23:01:27 +0200

    first commit

--- sources/views/top.js
+++ sources/views/top.js
@@ -119,6 +119,7 @@ export default class TopView extends JetView{
             { value:"Ткани", id:"cloth-directory", icon:"mdi mdi-circle-outline" },
             { value:"Сотрудники", id:"employee-directory", icon:"mdi mdi-circle-outline" },
             { value:"Отделы", id:"department-directory", icon:"mdi mdi-circle-outline" },
+            { value:"Типы зарплат", id:"employee-salary-type-directory", icon:"mdi mdi-circle-outline" },
 
           ]
 				},
