5c810640cd0b4834d02dbce7580ce78054794213
a_dulsky <andrey.dulsky@gmail.com>
2021-01-26 01:41:15 +0200

    first commit

--- sources/views/employee-directory/index.js
+++ sources/views/employee-directory/index.js
@@ -94,7 +94,7 @@ export default class EmployeeDirectoryView extends JetView{
                 },
                 {"id": "action-edit", "header": "", "width": 50, "template": "{common.editIcon()}"}
               ],
-              url: this.app.config.apiRest.getUrl('get',"accounting/employees", {'sort':'department_id,name', 'expand': 'department'}),//"api->accounting/contragents",
+              url: this.app.config.apiRest.getUrl('get',"accounting/employees", {'sort':'department_id', 'expand': 'department'}),//"api->accounting/contragents",
               save: "api->accounting/employees",
 
               scheme: {
