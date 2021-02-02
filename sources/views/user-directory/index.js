2f354bc614cdfa794d27260c7cb3e65ad841e43a
a_dulsky <andrey.dulsky@gmail.com>
2021-02-02 21:12:10 +0200

    first commit

---
+++ sources/views/user-directory/index.js
@@ -0,0 +1,204 @@
+import {JetView} from "webix-jet";
+import UpdateFormView from "core/updateFormView";
+//import {departments} from "models/department/departments";
+//import {typeSalary} from "models/department/type-salary";
+import "components/comboClose";
+import "components/comboDateClose";
+import "components/searchClose";
+
+
+export default class UserDirectoryView extends JetView{
+  config(){
+    let scope = this;
+    return {
+      localId: "layout",
+      type:"wide",
+      cols:[
+        {
+          rows: [
+
+
+            {
+              "view": "toolbar",
+              height: 40,
+              paddingY:2,
+              cols: [
+                {
+                  "view": "label",
+                  "label": "Сотрудники",
+                  "width": 150
+                },
+
+                {},
+                { "label": "", "view": "search-close", "width": 300,  "align" :"right", localId: 'form-search'  },
+                {
+                  view:"icon",
+                  icon:"mdi mdi-fullscreen",
+                  width: 30,
+                  click: function() {
+                    webix.fullscreen.set(this.$scope.$$("user-table"));
+                  }
+                },
+              ]
+            },
+            {
+              "view": "toolbar",
+              "height": 40,
+              "paddingY":2,
+              "cols": [
+                {
+                  "label": "Добавить",
+                  "type":"icon",
+                  "icon":"mdi mdi-plus",
+                  "view": "button",
+                  "height": 50,
+                  "css": "webix_primary",
+                  //"width": 120,
+                  autowidth:true,
+                  click: () => this.doAddClick()
+                },
+                {},
+                {
+                  view:"toggle",
+                  type:"icon",
+                  icon: 'mdi mdi-file-tree',
+                  autowidth:true,
+                  value :true,
+                  click: function() { scope.doClickOpenAll() }
+
+                },
+
+              ]
+            },
+            {
+              view: "treetable",
+              localId: "user-table",
+              urlEdit: 'user',
+              //autoConfig: true,
+              css:"webix_header_border webix_data_border",
+              //leftSplit:1,
+              //rightSplit:2,
+              select: true,
+              //datafetch:100,
+              //datathrottle: 500,
+              //loadahead:100,
+              resizeColumn: { headerOnly:true },
+
+              columns:[
+                { id:"id", header:"ID", width: 40 },
+                { id:"username", header:"Логин", width: 150},
+                { id:"email", header:"E-mail", width: 200},
+                { id:"status", header:"Статус", width: 80, sort: "string", collection: [{'id':'10',"value":'Активен'},{'id':'0',"value":'Неактивен'}] },
+                { id:"type", header:"Тип прав", width: 80, sort: "string" },
+                { id:"last_login", header:"Последний заход", width: 140, sort: "string" },
+                { id:"login_ip", header:"IP адрес", width: 120, sort: "string" },
+                { id:"created_at", header:"Создан", width: 140, sort: "string" },
+                {
+                  "id": "action-delete",
+                  "header": "",
+                  "width": 50,
+                  "template": "{common.trashIcon()}"
+                },
+                {"id": "action-edit", "header": "", "width": 50, "template": "{common.editIcon()}"}
+              ],
+              url: this.app.config.apiRest.getUrl('get',"accounting/users", {'sort':'name'}),//"api->accounting/contragents",
+              save: "api->accounting/users",
+
+              scheme: {
+                // $sort:{ by:"department_id", dir:"asc", as:"int" },
+                $init:function(obj){ obj.index = this.count()+1; }
+              },
+              ready:function(){
+                this.openAll();
+              },
+              on:{
+                onItemClick:function(id, e, trg) {
+
+                  if (id.column == 'action-delete') {
+                    var table = this;
+                    webix.confirm("Удалить запись?").then(function(result){
+                      webix.dp(table).save(
+                        id.row,
+                        "delete"
+                      ).then(function(obj){
+                        webix.dp(table).ignore(function(){
+                          table.remove(id.row);
+                        });
+                      }, function(){
+                        webix.message("Ошибка! Запись не удалена!");
+                      });
+                    });
+
+                  } else {
+                    this.$scope.cashEdit.showForm(this);
+                  }
+                },
+                onBeforeLoad:function(){
+                  this.showOverlay("Loading...");
+                },
+                onAfterLoad:function(){
+                  if (!this.count())
+                    this.showOverlay("Sorry, there is no data");
+                  else
+                    this.hideOverlay();
+                },
+              }
+            }
+          ]
+        },
+
+      ]
+    };
+  }
+
+  init(view){
+
+    let form = this.$$("form-search");
+    let table = this.$$("user-table");
+    //table.markSorting("name", "asc");
+    let scope = this;
+
+
+    form.attachEvent("onChange", function(obj){
+      let filter = {'search':form.getValue()};
+      let objFilter = { filter: filter };
+
+      webix.extend(table, webix.ProgressBar);
+
+      table.clearAll(true);
+      table.showProgress({
+        delay:2000,
+        hide:false
+      });
+
+      webix.ajax().get( scope.app.config.apiRest.getUrl('get','accounting/users', {'sort':'name'}), objFilter).then(function(data) {
+        table.parse(data);
+        table.openAll();
+      });
+
+
+      // table.loadNext(0, 0, 0, 0, 1).then(function (data) {
+      //     table.clearAll(true);
+      //     table.parse(data);
+      // });
+
+    });
+
+    this.cashEdit = this.ui(UpdateFormView);
+  }
+
+  doAddClick() {
+    this.$$('user-table').unselect();
+    this.cashEdit.showForm(this.$$('user-table'));
+  }
+
+  doClickOpenAll() {
+    let table = this.$$("user-table");
+    if (table.getOpenItems().length >0 ) {
+      table.closeAll();
+    } else {
+      table.openAll();
+    }
+  }
+
+}
